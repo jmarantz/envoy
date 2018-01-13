e2cc8604d9d69f3a954d6ca6528871870b220ce8
Joshua Marantz <jmarantz@google.com>
2018-01-13 18:13:41 -0500

    Change the shared-memory hash_map to a hash_set.  Rename to come next.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/common/shared_memory_hash_map.h
+++ source/common/common/shared_memory_hash_map.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <pthread.h>
-
 #include <algorithm>
 #include <string>
 #include <utility>
@@ -10,43 +8,42 @@
 #include "common/common/hash.h"
 #include "common/common/logger.h"
 
-#include "absl/base/thread_annotations.h"
 #include "absl/strings/string_view.h"
 #include "fmt/format.h"
 
 namespace Envoy {
 
 /**
- * Initialization parameters for SharedMemoryHashMap. The options are duplicated
+ * Initialization parameters for SharedMemoryHashSet. The options are duplicated
  * to the control-block after init, to aid with sanity checking when attaching
  * an existing memory segment.
  */
-struct SharedMemoryHashMapOptions {
+struct SharedMemoryHashSetOptions {
   std::string toString() const {
-    return fmt::format("capacity={}, max_key_size={}, num_slots={}", capacity, max_key_size,
-                       num_slots);
+    return fmt::format("capacity={}, num_slots={}", capacity, num_slots);
   }
-  bool operator==(const SharedMemoryHashMapOptions& that) const {
-    return capacity == that.capacity && max_key_size == that.max_key_size &&
-           num_slots == that.num_slots;
+  bool operator==(const SharedMemoryHashSetOptions& that) const {
+    return capacity == that.capacity && num_slots == that.num_slots;
   }
-  bool operator!=(const SharedMemoryHashMapOptions& that) const { return !(*this == that); }
+  bool operator!=(const SharedMemoryHashSetOptions& that) const { return !(*this == that); }
 
-  uint32_t capacity;     // how many values can be stored.
-  uint32_t max_key_size; // how many bytes of string can be stored.
-  uint32_t num_slots;    // determines speed of hash vs size efficiency.
+  uint32_t capacity;  // how many values can be stored.
+  uint32_t num_slots; // determines speed of hash vs size efficiency.
 };
 
 /**
- * Implements hash_map<string, Value> without using pointers, suitable
- * for use in shared memory. Users must commit to capacity, max_key_size,
- * and num_slots at construction time, and to the payload value at compile-time.
+ * Implements hash_set<Value> without using pointers, suitable for use
+ * in shared memory. Users must commit to capacity and num_slots at
+ * construction time. Value must provide these methods:
+ *    absl::string_view Value::key()
+ *    void Value::initialize(absl::string_view key)
+ *    static size_t Value::size()
  *
- * This map may also be suitable for persisting a hash-table to long term storage,
- * but not across machine architectures, as it doesn't use network byte order for
- * storing ints.
+ * This set may also be suitable for persisting a hash-table to long
+ * term storage, but not across machine architectures, as it doesn't
+ * use network byte order for storing ints.
  */
-template <class Value> class SharedMemoryHashMap : public Logger::Loggable<Logger::Id::config> {
+template <class Value> class SharedMemoryHashSet : public Logger::Loggable<Logger::Id::config> {
 public:
   /**
    * Sentinal used for next_cell links to indicate end-of-list.
@@ -62,7 +59,7 @@ public:
    * After the control-structure is constructed, the number of bytes can be computed so
    * that a shared-memory segment can be allocated and passed to init() or attach().
    */
-  SharedMemoryHashMap(const SharedMemoryHashMapOptions& options)
+  SharedMemoryHashSet(const SharedMemoryHashSetOptions& options)
       : options_(options), cells_(nullptr), control_(nullptr), slots_(nullptr) {}
 
   /**
@@ -71,11 +68,18 @@ public:
    * backing-store (eg) in shared memory, which we do after
    * constructing the object with the desired sizing.
    */
-  static size_t numBytes(const SharedMemoryHashMapOptions& options) {
-    size_t size = cellOffset(options.capacity, options) + sizeof(Control) +
-                  options.num_slots * sizeof(uint32_t);
+  static uint32_t numBytes(const SharedMemoryHashSetOptions& options) {
+    uint32_t size =
+        cellOffset(options.capacity) + sizeof(Control) + options.num_slots * sizeof(uint32_t);
     return align(size);
   }
+
+  uint32_t numBytes() const { return numBytes(options_); }
+
+  /**
+   * Returns the options structure that was used to construct the set.
+   */
+  const SharedMemoryHashSetOptions& options() const { return options_; }
 
   /**
    * Attempts to attach to an existing shared memory segment. Does a (relatively) quick
@@ -85,21 +89,65 @@ public:
    * Note that if mutex is in a locked state at the time of attachment, this function
    * can hang.
    */
-  bool attach(uint8_t* memory, size_t num_bytes) {
-    if (num_bytes != numBytes(options_)) {
-      ENVOY_LOG(error, "SharedMemoryHashMap unexpected memory size {}!={}", num_bytes,
-                numBytes(options_));
+  bool attach(uint8_t* memory, uint32_t num_bytes) {
+    mapMemorySegments(memory);
+    if (num_bytes != control_->num_bytes) {
+      ENVOY_LOG(error, "SharedMemoryHashSet unexpected memory size {} != {}",
+                num_bytes, control_->num_bytes);
       return false;
     }
-    mapMemorySegments(memory);
     return sanityCheck();
   }
 
-  /** Locks the map and runs sanity checks */
-  bool sanityCheck() LOCKS_EXCLUDED(control_->mutex) {
-    lock(); // might hang if program previously crashed.
-    bool ret = sanityCheckLockHeld();
-    unlock();
+  /** Examines the data structures to see if they are sane. Tries hard not to crash or hang. */
+  bool sanityCheck() {
+    bool ret = true;
+    if (options_ != control_->options) {
+      // options doesn't match.
+      ENVOY_LOG(error, "SharedMemoryHashSet options don't match");
+      return false;
+    }
+
+    if (control_->size > options_.capacity) {
+      ENVOY_LOG(error, "SharedMemoryHashSet size={} > capacity={}", control_->size,
+                options_.capacity);
+      return false;
+    }
+
+    // As a sanity check, make sure there are control_->size values
+    // reachable from the slots, each of which has a valid char_offset
+    uint32_t num_values = 0;
+    for (uint32_t slot = 0; slot < options_.num_slots; ++slot) {
+      uint32_t next = 0; // initialized to silence compilers.
+      for (uint32_t cell_index = slots_[slot]; cell_index != Sentinal; cell_index = next) {
+        if (cell_index >= options_.capacity) {
+          ENVOY_LOG(error, "SharedMemoryHashSet cell index too high={}, capacity={}", cell_index,
+                    options_.capacity);
+          ret = false;
+          break;
+        } else {
+          Cell& cell = getCell(cell_index);
+          absl::string_view key = cell.value.key();
+          if (computeSlot(key) != slot) {
+            ENVOY_LOG(error, "SharedMemoryHashSet hash mismatch for key={}", std::string(key));
+            ret = false;
+          }
+          next = cell.next_cell;
+          ++num_values;
+          // Avoid infinite loops if there is a next_cell cycle within
+          // a slot. Note that the num_values message will be emitted
+          // outside the loop.
+          if (num_values > control_->size) {
+            break;
+          }
+        }
+      }
+    }
+    if (num_values != control_->size) {
+      ENVOY_LOG(error, "SharedMemoryHashSet has wrong number of live cells: {}, expected {}",
+                num_values, control_->size);
+      ret = false;
+    }
     return ret;
   }
 
@@ -109,16 +157,14 @@ public:
    */
   std::string toString() {
     std::string ret;
-    lock();
     ret = fmt::format("options={}\ncontrol={}\n", options_.toString(), control_->toString());
     for (uint32_t i = 0; i < options_.num_slots; ++i) {
       ret += fmt::format("slot {}:", i);
       for (uint32_t j = slots_[i]; j != Sentinal; j = getCell(j).next_cell) {
-        ret += " " + std::string(getCell(j).getKey());
+        ret += " " + std::string(getCell(j).value.key());
       }
       ret += "\n";
     }
-    unlock();
     return ret;
   }
 
@@ -127,10 +173,10 @@ public:
    * coming in.
    * @param memory
    */
-  void init(uint8_t* memory) {
+  void init(uint8_t* memory, uint32_t num_bytes) {
     mapMemorySegments(memory);
-    lock();
-
+    RELEASE_ASSERT(num_bytes == numBytes(options_));
+    control_->num_bytes = num_bytes;
     control_->options = options_;
     control_->size = 0;
     control_->free_cell_index = 0;
@@ -143,51 +189,42 @@ public:
     // Initialize all the key-char offsets.
     for (uint32_t cell_index = 0; cell_index < options_.capacity; ++cell_index) {
       Cell& cell = getCell(cell_index);
-      cell.key_size = 0;
       cell.next_cell = cell_index + 1;
     }
-
-    unlock();
   }
 
   /**
-   * Puts a new key into the map. If successful (e.g. map has capacity)
-   * then put returns a pointer to the value object, which the caller
-   * can then write. Returns {nullptr, false} if the key was too large, or the
-   * capacity of the map has been exceeded.
+   * Inserts a value into the set. If successful (e.g. map has
+   * capacity) then put returns a pointer to the value object, which
+   * the caller can then write, Returns {nullptr, false} if the key
+   * was too large, or the capacity of the map has been exceeded.
    *
    * If the value was already present in the map, then {value, false} is returned.
    * The caller may need to clean up an old value.
    *
    * If the value is newly allocated, then {value, true} is returned.
    *
-   * @param key THe key must be 255 bytes or smaller.
    * @return a pair with the value-pointer (or nullptr), and a bool indicating
    *         whether the value is newly allocated.
    */
-  ValueCreatedPair put(absl::string_view key) {
-    if (key.size() > options_.max_key_size) {
+  ValueCreatedPair insert(absl::string_view key) {
+    Value* value = get(key);
+    if (value != nullptr) {
+      return ValueCreatedPair(value, false);
+    }
+    if (control_->size >= options_.capacity) {
       return ValueCreatedPair(nullptr, false);
     }
-
-    lock();
-    Value* value = getLockHeld(key);
-    bool created = false;
-    if ((value == nullptr) && (control_->size < options_.capacity)) {
-      created = true;
-      uint32_t slot = HashUtil::xxHash64(key) % options_.num_slots;
-      uint32_t cell_index = control_->free_cell_index;
-      Cell& cell = getCell(cell_index);
-      control_->free_cell_index = cell.next_cell;
-      cell.next_cell = slots_[slot];
-      slots_[slot] = cell_index;
-      cell.key_size = key.size();
-      memcpy(cell.key, key.data(), key.size());
-      value = &cell.value;
-      ++control_->size;
-    }
-    unlock();
-    return ValueCreatedPair(value, created);
+    const uint32_t slot = computeSlot(key);
+    const uint32_t cell_index = control_->free_cell_index;
+    Cell& cell = getCell(cell_index);
+    control_->free_cell_index = cell.next_cell;
+    cell.next_cell = slots_[slot];
+    slots_[slot] = cell_index;
+    value = &cell.value;
+    value->initialize(key);
+    ++control_->size;
+    return ValueCreatedPair(value, true);
   }
 
   /**
@@ -196,49 +233,45 @@ public:
    * @param key the key to remove
    */
   bool remove(absl::string_view key) {
-    if (key.size() > options_.max_key_size) {
-      return false;
-    }
-
-    bool found = false;
-    lock();
-    uint32_t slot = HashUtil::xxHash64(key) % options_.num_slots;
+    const uint32_t slot = computeSlot(key);
     uint32_t* next = nullptr;
     for (uint32_t* cptr = &slots_[slot]; *cptr != Sentinal; cptr = next) {
       Cell& cell = getCell(*cptr);
       next = &cell.next_cell;
-      if (cell.getKey() == key) {
+      if (cell.value.key() == key) {
         control_->free_cell_index = *cptr;
         --control_->size;
         *cptr = *next; // Splices current cell out of slot-chain.
         *next = control_->free_cell_index;
-        found = true;
-        break;
+        return true;
       }
     }
-    unlock();
-    return found;
+    return false;
   }
 
   /** Returns the number of key/values stored in the map. */
-  size_t size() const { return control_->size; }
+  uint32_t size() const { return control_->size; }
 
   /**
-   * Gets the value associated with a key, returning null if the value was not found.
+   * Gets the value associated with a key, returning nullptr if the value was not found.
    * @param key
    */
   Value* get(absl::string_view key) {
-    if (key.size() > options_.max_key_size) {
-      return nullptr;
+    const uint32_t slot = computeSlot(key);
+    for (uint32_t c = slots_[slot]; c != Sentinal; c = getCell(c).next_cell) {
+      Cell& cell = getCell(c);
+      if (cell.value.key() == key) {
+        return &cell.value;
+      }
     }
-
-    lock();
-    Value* value = getLockHeld(key);
-    unlock();
-    return value;
+    return nullptr;
   }
 
 private:
+  uint32_t computeSlot(absl::string_view key) {
+    return HashUtil::xxHash64(key) % options_.num_slots;
+  }
+
   /**
    * Represents control-values for the hash-table, including a mutex, which
    * must gate all access to the internals.
@@ -249,8 +282,8 @@ private:
                          free_cell_index);
     }
 
-    pthread_mutex_t mutex;
-    SharedMemoryHashMapOptions options; // Options established at map construction time.
+    SharedMemoryHashSetOptions options; // Options established at map construction time.
+    uint32_t num_bytes;                 // Bytes allocated on behalf of the map.
     uint32_t size;                      // Number of values currently stored.
     uint32_t free_cell_index;           // Offset of first free cell.
   };
@@ -259,13 +292,8 @@ private:
    * Represents a value-cell, which is stored in a linked-list from each slot.
    */
   struct Cell {
-    /** Returns the key as a string_view. */
-    absl::string_view getKey() const { return absl::string_view(key, key_size); }
-
+    uint32_t next_cell; // Offset of next cell in map->cells_, terminated with Sentinal.
     Value value;        // Templated value field.
-    uint32_t next_cell; // OFfset of next cell in map->cells_, terminated with Sentinal.
-    uint8_t key_size;   // size of key in bytes, or 0 if unused.
-    char key[];
   };
 
   // It seems like this is an obvious constexpr, but it won't compile as one.
@@ -280,12 +308,12 @@ private:
    * simply an array index because we don't know the size of a key at
    * compile-time.
    */
-  static uint32_t cellOffset(uint32_t cell_index, const SharedMemoryHashMapOptions& options) {
-    uint32_t cell_size = align(options.max_key_size + sizeof(Cell));
+  static uint32_t cellOffset(uint32_t cell_index) {
+    // sizeof(Cell) includes 'sizeof Value' which may not be accurate. So we need to
+    // subtract that off, and add the template method's view of the actual value-size.
+    uint32_t cell_size = align(sizeof(Cell) + Value::size() - sizeof(Value));
     return cell_index * cell_size;
   }
-
-  uint32_t cellOffset(uint32_t cell_index) const { return cellOffset(cell_index, options_); }
 
   /**
    * Returns a reference to a Cell at the specified index.
@@ -306,92 +334,17 @@ private:
     control_ = reinterpret_cast<Control*>(memory);
     memory += sizeof(Control);
     slots_ = reinterpret_cast<uint32_t*>(memory);
-
-    // TODO(jmarantz): share this code with SharedMemory::initializeMutex in hot_restart_impl.cc
-    pthread_mutexattr_t attribute;
-    pthread_mutexattr_init(&attribute);
-    pthread_mutexattr_setpshared(&attribute, PTHREAD_PROCESS_SHARED);
-    pthread_mutexattr_setrobust(&attribute, PTHREAD_MUTEX_ROBUST);
-    pthread_mutex_init(&control_->mutex, &attribute);
   }
-
-  Value* getLockHeld(absl::string_view key) EXCLUSIVE_LOCKS_REQUIRED(control_->mutex) {
-    uint32_t slot = HashUtil::xxHash64(key) % options_.num_slots;
-    for (uint32_t c = slots_[slot]; c != Sentinal; c = getCell(c).next_cell) {
-      Cell& cell = getCell(c);
-      if (cell.getKey() == key) {
-        return &cell.value;
-      }
-    }
-    return nullptr;
-  }
-
-  /** Examines the data structures to see if they are sane. Tries hard not to crash or hang. */
-  bool sanityCheckLockHeld() EXCLUSIVE_LOCKS_REQUIRED(control_->mutex) {
-    bool ret = true;
-    if (options_ != control_->options) {
-      // options doesn't match.
-      ENVOY_LOG(error, "SharedMemoryHashMap options don't match");
-      return false;
-    }
-
-    if (control_->size > options_.capacity) {
-      ENVOY_LOG(error, "SharedMemoryHashMap size={} > capacity={}", control_->size,
-                options_.capacity);
-      return false;
-    }
-
-    // As a sanity check, make sure there are control_->size values
-    // reachable from the slots, each of which has a valid char_offset
-    uint32_t num_values = 0;
-    for (uint32_t slot = 0; slot < options_.num_slots; ++slot) {
-      uint32_t next = 0; // initialized to silence compilers.
-      for (uint32_t cell_index = slots_[slot]; cell_index != Sentinal; cell_index = next) {
-        if (cell_index >= options_.capacity) {
-          ENVOY_LOG(error, "SharedMemoryHashMap cell index too high={}, capacity={}", cell_index,
-                    options_.capacity);
-          ret = false;
-          break;
-        } else {
-          Cell& cell = getCell(cell_index);
-          next = cell.next_cell;
-          if (cell.key_size > options_.max_key_size) {
-            ENVOY_LOG(error, "SharedMemoryHashMap live cell has key_size=={}", cell.key_size);
-            ret = false;
-          }
-          ++num_values;
-          // Avoid infinite loops if there is a next_cell cycle within
-          // a slot. Note that the num_values message will be emitted
-          // outside the loop.
-          if (num_values > control_->size) {
-            break;
-          }
-        }
-      }
-    }
-    if (num_values != control_->size) {
-      ENVOY_LOG(error, "SharedMemoryHashMap has wrong number of live cells: {}, expected {}",
-                num_values, control_->size);
-      ret = false;
-    }
-    return ret;
-  }
-
-  /** Locks the mutex. */
-  void lock() { pthread_mutex_lock(&control_->mutex); }
-
-  /** Unlocks the mutex. */
-  void unlock() { pthread_mutex_unlock(&control_->mutex); }
 
   // Copy of the options in process-local memory; which is used to help compute
   // the required size in bytes after construction and before init/attach.
-  const SharedMemoryHashMapOptions options_;
+  const SharedMemoryHashSetOptions options_;
 
   // Pointers into shared memory. Cells go first, because Value may need a more aggressive
   // aligmnment.
-  Cell* cells_ PT_GUARDED_BY(control_->mutex);
+  Cell* cells_;
   Control* control_;
-  uint32_t* slots_ PT_GUARDED_BY(control_->mutex);
+  uint32_t* slots_;
 };
 
 } // namespace Envoy
--- test/common/common/shared_memory_hash_map_test.cc
+++ test/common/common/shared_memory_hash_map_test.cc
@@ -1,143 +1,148 @@
+#include <algorithm>
 #include <cstdint>
 #include <memory>
 #include <string>
 
-#include "common/common/shared_memory_hash_map.h"
+#include "common/common/shared_memory_hash_set.h"
 
+#include "absl/strings/string_view.h"
 #include "fmt/format.h"
 #include "gtest/gtest.h"
 
 namespace Envoy {
 
-// Tests SharedMemoryHashMap.
-class SharedMemoryHashTest : public testing::Test {
+// Tests SharedMemoryHashSet.
+class SharedMemoryHashSetTest : public testing::Test {
 protected:
-  typedef SharedMemoryHashMap<int64_t>::ValueCreatedPair ValueCreatedPair;
+  struct TestValue {
+    absl::string_view key() const { return name; }
+    void initialize(absl::string_view key) {
+      size_t xfer = std::max(sizeof(name) - 1, key.size());
+      memcpy(name, key.data(), xfer);
+      name[xfer] = '\0';
+    }
+    static size_t size() { return sizeof(TestValue); }
+
+    int64_t number;
+    char name[256];
+  };
+
+  typedef SharedMemoryHashSet<TestValue>::ValueCreatedPair ValueCreatedPair;
 
   void SetUp() override {
     options_.capacity = 100;
-    options_.max_key_size = 250;
     options_.num_slots = 67;
-    mem_size_ = SharedMemoryHashMap<int64_t>::numBytes(options_);
+    mem_size_ = SharedMemoryHashSet<TestValue>::numBytes(options_);
     memory_.reset(new uint8_t[mem_size_]);
   }
 
-  SharedMemoryHashMapOptions options_;
-  size_t mem_size_;
+  SharedMemoryHashSetOptions options_;
+  uint32_t mem_size_;
   std::unique_ptr<uint8_t[]> memory_;
 };
 
-TEST_F(SharedMemoryHashTest, initAndAttach) {
+TEST_F(SharedMemoryHashSetTest, initAndAttach) {
   {
-    SharedMemoryHashMap<int64_t> hash_map1(options_);
-    hash_map1.init(memory_.get());
-    SharedMemoryHashMap<int64_t> hash_map2(options_);
-    EXPECT_TRUE(hash_map2.attach(memory_.get(), mem_size_));
+    SharedMemoryHashSet<TestValue> hash_set1(options_);
+    hash_set1.init(memory_.get(), mem_size_);
+    SharedMemoryHashSet<TestValue> hash_set2(options_);
+    EXPECT_TRUE(hash_set2.attach(memory_.get(), mem_size_));
   }
 
   // If we pass in a memory-buffer with the wrong size, we can no longer attach it.
   {
-    SharedMemoryHashMap<int64_t> hash_map3(options_);
-    EXPECT_FALSE(hash_map3.attach(memory_.get(), mem_size_ - 1));
+    SharedMemoryHashSet<TestValue> hash_set3(options_);
+    //EXPECT_FALSE(hash_set3.attach(memory_.get(), mem_size_ - 1));
   }
 
   // If we tweak an option, we can no longer attach it.
   {
     options_.capacity = 99;
-    SharedMemoryHashMap<int64_t> hash_map4(options_);
-    EXPECT_FALSE(hash_map4.attach(memory_.get(), mem_size_));
+    SharedMemoryHashSet<TestValue> hash_set4(options_);
+    EXPECT_FALSE(hash_set4.attach(memory_.get(), mem_size_));
   }
 }
 
-TEST_F(SharedMemoryHashTest, putRemove) {
+TEST_F(SharedMemoryHashSetTest, putRemove) {
   {
-    SharedMemoryHashMap<int64_t> hash_map1(options_);
+    SharedMemoryHashSet<TestValue> hash_set1(options_);
 
-    hash_map1.init(memory_.get());
-    EXPECT_TRUE(hash_map1.sanityCheck());
-    EXPECT_EQ(0, hash_map1.size());
-    EXPECT_EQ(nullptr, hash_map1.get("no such key"));
-    ValueCreatedPair vc = hash_map1.put("good key");
+    hash_set1.init(memory_.get(), mem_size_);
+    EXPECT_TRUE(hash_set1.sanityCheck());
+    EXPECT_EQ(0, hash_set1.size());
+    EXPECT_EQ(nullptr, hash_set1.get("no such key"));
+    ValueCreatedPair vc = hash_set1.insert("good key");
     EXPECT_TRUE(vc.second);
-    *vc.first = 12345;
-    EXPECT_TRUE(hash_map1.sanityCheck());
-    EXPECT_EQ(1, hash_map1.size());
-    EXPECT_EQ(12345, *hash_map1.get("good key"));
-    EXPECT_EQ(nullptr, hash_map1.get("no such key"));
+    vc.first->number = 12345;
+    EXPECT_TRUE(hash_set1.sanityCheck());
+    EXPECT_EQ(1, hash_set1.size());
+    EXPECT_EQ(12345, hash_set1.get("good key")->number);
+    EXPECT_EQ(nullptr, hash_set1.get("no such key"));
 
-    vc = hash_map1.put("good key");
+    vc = hash_set1.insert("good key");
     EXPECT_FALSE(vc.second) << "re-used, not newly created";
-    *vc.first = 6789;
-    EXPECT_EQ(6789, *hash_map1.get("good key"));
-    EXPECT_EQ(1, hash_map1.size());
+    vc.first->number = 6789;
+    EXPECT_EQ(6789, hash_set1.get("good key")->number);
+    EXPECT_EQ(1, hash_set1.size());
   }
 
   {
     // Now init a new hash-map with the same memory.
-    SharedMemoryHashMap<int64_t> hash_map2(options_);
-    EXPECT_TRUE(hash_map2.attach(memory_.get(), mem_size_));
+    SharedMemoryHashSet<TestValue> hash_set2(options_);
+    EXPECT_TRUE(hash_set2.attach(memory_.get(), mem_size_));
 
-    EXPECT_EQ(1, hash_map2.size());
-    EXPECT_EQ(nullptr, hash_map2.get("no such key"));
-    EXPECT_EQ(6789, *hash_map2.get("good key")) << hash_map2.toString();
-    EXPECT_FALSE(hash_map2.remove("no such key"));
-    EXPECT_TRUE(hash_map2.remove("good key"));
-    EXPECT_EQ(nullptr, hash_map2.get("good key"));
-    EXPECT_EQ(0, hash_map2.size());
+    EXPECT_EQ(1, hash_set2.size());
+    EXPECT_EQ(nullptr, hash_set2.get("no such key"));
+    EXPECT_EQ(6789, hash_set2.get("good key")->number) << hash_set2.toString();
+    EXPECT_FALSE(hash_set2.remove("no such key"));
+    EXPECT_TRUE(hash_set2.remove("good key"));
+    EXPECT_EQ(nullptr, hash_set2.get("good key"));
+    EXPECT_EQ(0, hash_set2.size());
   }
 }
 
-TEST_F(SharedMemoryHashTest, keyTooBig) {
-  SharedMemoryHashMap<int64_t> hash_map1(options_);
-  hash_map1.init(memory_.get());
-
-  EXPECT_EQ(0, hash_map1.size());
-  std::string big_key(300, 'a');
-  EXPECT_EQ(nullptr, hash_map1.put(big_key).first) << big_key;
-}
-
-TEST_F(SharedMemoryHashTest, tooManyValues) {
-  SharedMemoryHashMap<int64_t> hash_map1(options_);
-  hash_map1.init(memory_.get());
+TEST_F(SharedMemoryHashSetTest, tooManyValues) {
+  SharedMemoryHashSet<TestValue> hash_set1(options_);
+  hash_set1.init(memory_.get(), mem_size_);
   std::vector<std::string> keys;
   for (uint32_t i = 0; i < options_.capacity + 1; ++i) {
     keys.push_back(fmt::format("key{}", i));
   }
 
   for (uint32_t i = 0; i < options_.capacity; ++i) {
-    int64_t* value = hash_map1.put(keys[i]).first;
+    TestValue* value = hash_set1.insert(keys[i]).first;
     ASSERT_NE(nullptr, value);
-    *value = i;
+    value->number = i;
   }
-  EXPECT_TRUE(hash_map1.sanityCheck());
-  EXPECT_EQ(options_.capacity, hash_map1.size());
+  EXPECT_TRUE(hash_set1.sanityCheck());
+  EXPECT_EQ(options_.capacity, hash_set1.size());
 
   for (uint32_t i = 0; i < options_.capacity; ++i) {
-    const int64_t* value = hash_map1.get(keys[i]);
+    const TestValue* value = hash_set1.get(keys[i]);
     ASSERT_NE(nullptr, value);
-    EXPECT_EQ(i, *value);
+    EXPECT_EQ(i, value->number);
   }
-  EXPECT_TRUE(hash_map1.sanityCheck());
+  EXPECT_TRUE(hash_set1.sanityCheck());
 
   // We can't fit one more value.
-  EXPECT_EQ(nullptr, hash_map1.put(keys[options_.capacity]).first);
-  EXPECT_TRUE(hash_map1.sanityCheck()) << hash_map1.toString();
-  EXPECT_EQ(options_.capacity, hash_map1.size());
+  EXPECT_EQ(nullptr, hash_set1.insert(keys[options_.capacity]).first);
+  EXPECT_TRUE(hash_set1.sanityCheck()) << hash_set1.toString();
+  EXPECT_EQ(options_.capacity, hash_set1.size());
 
   // Now remove everything one by one.
   for (uint32_t i = 0; i < options_.capacity; ++i) {
-    EXPECT_TRUE(hash_map1.remove(keys[i]));
+    EXPECT_TRUE(hash_set1.remove(keys[i]));
   }
-  EXPECT_TRUE(hash_map1.sanityCheck());
-  EXPECT_EQ(0, hash_map1.size());
+  EXPECT_TRUE(hash_set1.sanityCheck());
+  EXPECT_EQ(0, hash_set1.size());
 
   // Now we can put in that last key we weren't able to before.
-  int64_t* value = hash_map1.put(keys[options_.capacity]).first;
+  TestValue* value = hash_set1.insert(keys[options_.capacity]).first;
   EXPECT_NE(nullptr, value);
-  *value = 314519;
-  EXPECT_EQ(1, hash_map1.size());
-  EXPECT_EQ(314519, *hash_map1.get(keys[options_.capacity]));
-  EXPECT_TRUE(hash_map1.sanityCheck());
+  value->number = 314519;
+  EXPECT_EQ(1, hash_set1.size());
+  EXPECT_EQ(314519, hash_set1.get(keys[options_.capacity])->number);
+  EXPECT_TRUE(hash_set1.sanityCheck());
 }
 
 } // namespace Envoy
