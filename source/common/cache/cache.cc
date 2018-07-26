3d8cf7bee78e12e52354f4c80c3ee6b70ed610bf
Joshua Marantz <jmarantz@google.com>
2018-07-25 23:04:56 -0400

    cache prototype passes very basic test.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

---
+++ source/common/cache/cache.cc
@@ -0,0 +1,23 @@
+#include "common/cache/cache.h"
+
+namespace Envoy {
+namespace Cache {
+
+Backend::Backend() : self_(this) {
+}
+
+Backend::~Backend() {
+  ASSERT(self_.get() == nullptr);
+}
+
+void Backend::multiLookup(const MultiLookupRequest&) {
+  ASSERT(false);
+}
+
+bool ValidStatus(DataStatus status) {
+  return status == DataStatus::kChunksImminent || status == DataStatus::kChunksPending ||
+    status == DataStatus::kLastChunk;
+}
+
+}  // namespace Cache
+}  // namespace Envoy
